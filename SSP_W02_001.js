aeac0e337730b21658c26fae88b03d31ecbd1edf
sinead.71 <cooneysinead71.com>
2016-01-26 16:00:02 +0000

    only just getting started on the next part of the assignment. only the first line written.

--- SSP_W02_001.js
+++ SSP_W02_001.js
@@ -3,22 +3,25 @@ console.log("hello world");
 //requesting the file system module
 var fs = require("fs");
 
-fs.readdir("./", myFileContents);
+fs.readdir("./", function(err, files){
+	 console.log("Contents of directory");
+	  for(var i in files){
+		  if(files[i] != "SSP_W02_001.js" ){
+		      console.log("File: " + files[i]);
+	 	      fs.readFile(files[i], {encoding:"utf8", flag:"r"}, function(error, data){
+			         console.log(data);
+               if(i == files.length -1){
+                   console.log("all done");
+               }
 
-function myFileContents(err, files){
-  console.log("Got the contents")
 
-  for (var i in files){
-    console.log("File:" + files[i]);
-
-    if(file[i] != "SSP_W01_001.js"){
-      fs.readFile(files[i], {encoding:"utf8", flag: "r"}, function(err, data){
-      if(error){
-        throw error;
+			    });
       }
+    }
+});
 
-      console.log("contents of " + file[i] + "/n/n" + data);
 
-     }
-   }
-}
+
+fs.readdir("./", function(err, files){
+
+});

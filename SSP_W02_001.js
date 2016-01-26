50ae2daa1e4ae068a6b0ddb0ccc5a2f61fb5b96d
sinead.71 <cooneysinead71.com>
2016-01-26 14:43:24 +0000

    Fixed code after explanation.

--- SSP_W02_001.js
+++ SSP_W02_001.js
@@ -5,12 +5,20 @@ var fs = require("fs");
 
 fs.readdir("./", myFileContents);
 
-fs.access("./", fs.R_OK, myFileContents);
-
 function myFileContents(err, files){
   console.log("Got the contents")
 
   for (var i in files){
     console.log("File:" + files[i]);
-  }
+
+    if(file[i] != "SSP_W01_001.js"){
+      fs.readFile(files[i], {encoding:"utf8", flag: "r"}, function(err, data){
+      if(error){
+        throw error;
+      }
+
+      console.log("contents of " + file[i] + "/n/n" + data);
+
+     }
+   }
 }

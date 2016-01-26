1f14cdf56fbed8db892e69a3cdb63fc49cec3955
sinead.71 <cooneysinead71.com>
2016-01-26 14:29:30 +0000

    trying to get the files read.

--- SSP_W02_001.js
+++ SSP_W02_001.js
@@ -1,1 +1,16 @@
 console.log("hello world");
+
+//requesting the file system module
+var fs = require("fs");
+
+fs.readdir("./", myFileContents);
+
+fs.access("./", fs.R_OK, myFileContents);
+
+function myFileContents(err, files){
+  console.log("Got the contents")
+
+  for (var i in files){
+    console.log("File:" + files[i]);
+  }
+}
